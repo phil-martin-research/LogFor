9f6537cc07fc1c1ea65ae92a060fe96df36a2124
unknown <philip.martin.1981@gmail.com>
2013-07-25 16:42:27 +0100

    made slight changes to richness script and added biomass figure

--- R_scripts/Richness_logging.R
+++ R_scripts/Richness_logging.R
@@ -29,11 +29,9 @@ Richness$Rare<-ifelse(Richness$Rar=="Rarefied - Area","R",Richness$Rare)
 Richness$Rare<-ifelse(Richness$Rar=="Rarefied - Individuals","R",Richness$Rare)
 Richness$Rare<-as.factor(Richness$Rare)
 
-qplot(Richness$MU,Richness$ML)
-
 #log ratio
 ROM<-escalc(data=Richness,measure="ROM",m2i=MU,sd2i=SDU,n2i=SSU,m1i=ML,sd1i=SDL,n1i=SSL,append=T)
-ROM
+
 
 #this runs a random effects meta-analysis for log ratio data
 ROM.ma<-rma.uni(yi,vi,method="REML",data=ROM)
@@ -49,7 +47,7 @@ plot2<-plot1+coord_flip()+geom_hline(aes(x=0), lty=2,size=1)
 plot3<-plot2+xlab("Study")+ylab("Proportional change")+scale_colour_manual(values=c("grey","black"))
 plot3+theme(legend.position="none")+scale_size_continuous(range=c(1,4))
 
-#model to look at 
+#model to look at differences between rarefied and not rarefied
 Model1<-rma.uni(yi,vi,mods=~Method+Rare+Tax,method="ML",data=ROM)
 summary(Model1)
 
@@ -59,13 +57,39 @@ summary(Model2)
 Model3<-rma.uni(yi,vi,mods=~Rare,method="ML",data=ROM)
 summary(Model3)
 
-plot(Model3)
 
-?rma.glmm
+model.res<-data.frame(Mean=c(-0.2943011,-0.2943011+0.3006),SE=c(0.0528,0.0730),Method=c("Unrarefied","Rarefied"))
 
-model.res<-data.frame(Mean=c(-0.2943011,-0.2943011+0.2703005),SE=c(0.0528,0.0648),Method=c("Unrarefied","Rarefied"))
+#plot the results of differences in rarefied
+Rare_plot<-ggplot(data=model.res,aes(y=Mean,x=Method,ymax=Mean+(SE*1.96),ymin=Mean-(SE*1.96)))+geom_pointrange(size=1.5)
+Rare_plot+coord_flip()+geom_hline(y=0,lty=2)
 
-ggplot(data=model.res,aes(y=Mean,x=Method,ymax=Mean+(SE*1.96),ymin=Mean-(SE*1.96)))+geom_pointrange()
+#plot the results of differences in taxa
+Rare_plot<-ggplot(data=model.res,aes(y=Mean,x=Method,ymax=Mean+(SE*1.96),ymin=Mean-(SE*1.96)))+geom_pointrange(size=1.5)
+Rare_plot+coord_flip()+geom_hline(y=0,lty=2)
+
+
+#subset to only include studies with data on volume
+Rich_vol<-subset(Richness,Vol>-9999)
+Rich_vol
+Rich_vol<-subset(Rich_vol,Rare=="R")
+#calculate the log ratio for these studies
+ROM_Rich<-escalc(data=Rich_vol,measure="ROM",m2i=MU,sd2i=SDU,n2i=SSU,m1i=ML,sd1i=SDL,n1i=SSL,append=T)
+
+
+#model to look at importance of volume
+Vol_M1<-rma.uni(yi,vi,mods=~Method+Vol,method="ML",data=ROM_Rich)
+summary(Vol_M1)
+
+Vol_M2<-rma.uni(yi,vi,mods=~Vol,method="ML",data=ROM_Rich)
+summary(Vol_M2)
+
+Vol_M3<-rma.uni(yi,vi,mods=~Rare+I(Vol/MU),method="ML",data=ROM_Rich)
+summary(Vol_M3)
+
+coef(Vol_M3)
+
+plot(Rich_vol$Vol,ROM_Rich$yi,col=Rich_vol$Rare)
 
 
 

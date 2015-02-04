50602d4b65c7b332a2021daf6b75e8b3db630663
Phil Martin <philip.martin.1981@gmail.com>
2015-02-04 12:12:47 +0100

    edited volume plot script to produce a plot of different volumes per region and by method

--- R_scripts/Volume_regions.R
+++ R_scripts/Volume_regions.R
@@ -1,48 +1,39 @@
+#######################################################################################
+#Script for plots showing variation in volume by location and for different############
+#logging methods#######################################################################
+#######################################################################################
+
+#name: Phil Martin
+#date:04/02/2015
+
+#clear objects
+rm(list=ls())
+
 #open packages
-library(RODBC)
 library(ggplot2)
-library(metafor)
-library(reshape2)
-
-#connect to database
-log <- odbcConnect("Logging")
-sqlTables(log)
-
-#import data
-Loc<- sqlFetch(log, "Location")
-head(Loc)
-colnames(Loc)<-c("Study","Site","Method","Vol","Lat","Long")
-
-#set contintent
-Loc$Cont<-ifelse(Loc$Long<(-30),"Americas",NA)
-Loc$Cont<-ifelse(Loc$Long>=-30&Loc$Long<=60,"Africa",Loc$Cont)
-Loc$Cont<-ifelse(Loc$Long>60,"SE Asia",Loc$Cont)
-Loc$Cont<-as.factor(Loc$Cont)
-
-tapply(Loc2$Vol,Loc2$Cont,mean)
+library(plyr)
 
 
-
-theme_set(theme_bw(base_size=26))
-a<-ggplot(data=Loc,aes(x=Long,y=Lat))+borders("world", size=0.1,colour="grey",fill="lightgrey")+theme(panel.grid.major = element_line(colour =NA))+geom_point(size=2,alpha=0.5)
-b<-a+coord_cartesian(xlim = c(-130, 180),ylim=c(-30, 30))
-b+geom_vline(x=c(-30,60))
-
-#how does intensity vary over the tropics
-Loc2<-subset(Loc,Vol>0)
-
-#standard error function
-std <- function(x) sd(x)/sqrt(length(x))
-
-#calculate mean
-
-Vols<-cbind(melt(tapply(Loc2$Vol,Loc2$Cont,mean)),(melt(tapply(Loc2$Vol,Loc2$Cont,std))[2]))
-colnames(Vols)<-c("Region","Vol","SE")
-
-ggplot(data=Vols,aes(x=Region,y=Vol,ymax=Vol+(1.96*SE),ymin=Vol-(1.96*SE)))+geom_pointrange(size=1.5)
+###########################################################################################
+#Organise data before analysis#############################################################
+###########################################################################################
 
 
-#map of regions
-a<-ggplot(data=Loc2,aes(x=Long,y=Lat))+borders("world", size=0.1,colour="grey",fill="lightgrey")+theme(panel.grid.major = element_line(colour =NA))+geom_point(aes(colour=Vol),alpha=0.5)
-b<-a+coord_cartesian(xlim = c(-130, 180),ylim=c(-30, 30))+theme(axis.text.x = element_blank(),axis.text.y = element_blank(), axis.ticks = element_blank(),axis.title.y = element_blank(),axis.title.x = element_blank())
-b+scale_size_area()
+setwd("C:/Users/Phil/Dropbox/Work/Active projects/PhD/Publications, Reports and Responsibilities/Chapters/5. Tropical forest degradation/Data/Fo analysis")
+Vol<-read.csv("Vol_var.csv")
+Vol2<-unique(Vol)
+
+head(Vol2)
+
+
+#plot variation in volume by region and by method
+theme_set(theme_bw(base_size=25))
+Vol_plot<-ggplot(Vol2,aes(y=Volume_pred,x=Region,fill=Method))+geom_boxplot()
+Vol_plot2<-Vol_plot+ylab(expression(paste("Volume of wood logged (",m^3,ha^-1,")")))+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
+Vol_plot2+scale_fill_brewer(palette="Set1")
+setwd("C:/Users/Phil/Dropbox/Work/Active projects/PhD/Publications, Reports and Responsibilities/Chapters/5. Tropical forest degradation/LogFor/Figures")
+ggsave("Vol_regions.png",height=6,width=10,dpi=400)
+
+tapply(x=Vol2$Volume_pred,INDEX =Vol2$Region,median,na.rm=TRUE)
+v<-c("Volume_pred")
+sapply(v, function(i) tapply(Vol2[[i]], Vol2$Region, mean, na.rm=TRUE))
